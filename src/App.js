9499bc03d0971f72996f2f559865b83c88d716b6
Tailer Nguyen <35902625+tailerchimchar@users.noreply.github.com>
2023-02-01 01:01:40 -0500

    adding initial progress bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import ProgressBar from "./progress-bar.component";
 
+const testData = [
+  { bgcolor: "#6a1b9a", completed: 60 },
+  { bgcolor: "#00695c", completed: 30 },
+  { bgcolor: "#ef6c00", completed: 53 },
+];
+
+function App() {
+  const [completed, setCompleted] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => setCompleted(Math.floor(Math.random() * 100) + 1), 2000);
+  }, []);
+
+  return (
+    <div className="App">
+      <ProgressBar bgcolor = {"red"} completed={completed}></ProgressBar>
+      <ProgressBar bgcolor={"#6a1b9a"} completed={completed} />
+    </div>
+    
+  );
+}
+
+/*
+function App() {
+  
+  return (
+    <div className="App">
+      {testData.map((item, idx) => (
+        <ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} />
+      ))}
+    </div>
+  );
+  
+}
+*/
+
+export default App;
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +61,6 @@ function App() {
     </div>
   );
 }
+*/
 
-export default App;
+//export default App;
